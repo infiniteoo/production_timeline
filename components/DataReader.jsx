3d27d92b9b9f68a6ee9ea0e5dd9dae5fe02303a4
Troy Dorman <troydorman@gmail.com>
2023-09-10 11:21:37 -0600

    separating the timelines

--- components/DataReader.jsx
+++ components/DataReader.jsx
@@ -2,15 +2,38 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const DataReader = () => {
-  const [excelData, setExcelData] = useState([]);
+  const [timelineA, setTimelineA] = useState([]);
+  const [timelineB, setTimelineB] = useState([]);
+  const [timelineC, setTimelineC] = useState([]);
 
   useEffect(() => {
     // Make an Axios GET request to fetch the Excel data
     axios
       .get("http://localhost:5000/api/excel")
-      .then((response) => {
-        console.log("Excel data:", response.data);
-        setExcelData(response.data);
+      .then(({ data }) => {
+        // Distribute data to timelines A, B, or C based on your logic
+        const updatedTimelineA = [];
+        const updatedTimelineB = [];
+        const updatedTimelineC = [];
+
+        data.forEach((row, index) => {
+          const entryA = row.slice(0, 6); // Entries 0-5
+          const entryB = [...row.slice(0, 3), ...row.slice(7, 10)]; // Entries 0-2 and 7-9
+          const entryC = [...row.slice(0, 3), ...row.slice(11, 14)]; // Entries 0-2 and 11-13
+
+          updatedTimelineA.push(entryA);
+          updatedTimelineB.push(entryB);
+          updatedTimelineC.push(entryC);
+        });
+
+        setTimelineA(updatedTimelineA);
+        setTimelineB(updatedTimelineB);
+        setTimelineC(updatedTimelineC);
+
+        // log all three updated timelines
+        console.log("Timeline A:", updatedTimelineA);
+        console.log("Timeline B:", updatedTimelineB);
+        console.log("Timeline C:", updatedTimelineC);
       })
       .catch((error) => {
         console.error("Error fetching the Excel data:", error);
@@ -20,6 +43,7 @@ const DataReader = () => {
   return (
     <div>
       <h2>Excel Data</h2>
+      <h3>Timeline A</h3>
       <table>
         <thead>
           <tr>
@@ -29,11 +53,51 @@ const DataReader = () => {
           </tr>
         </thead>
         <tbody>
-          {excelData.map((row, index) => (
+          {timelineA.map((row, index) => (
             <tr key={index}>
-              <td>{row["Column 1"]}</td>
-              <td>{row["Column 2"]}</td>
-              {/* Render more columns as needed */}
+              {row.map((entry, entryIndex) => (
+                <td key={entryIndex}>{entry}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h3>Timeline B</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            {/* Add more columns as needed */}
+          </tr>
+        </thead>
+        <tbody>
+          {timelineB.map((row, index) => (
+            <tr key={index}>
+              {row.map((entry, entryIndex) => (
+                <td key={entryIndex}>{entry}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h3>Timeline C</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            {/* Add more columns as needed */}
+          </tr>
+        </thead>
+        <tbody>
+          {timelineC.map((row, index) => (
+            <tr key={index}>
+              {row.map((entry, entryIndex) => (
+                <td key={entryIndex}>{entry}</td>
+              ))}
             </tr>
           ))}
         </tbody>
--- server/index.js
+++ server/index.js
@@ -27,8 +27,33 @@ app.use(express.static('public'))
 // Serve static files from the "public" directory
 app.use(express.static('public'))
 
+function excelDateToJSDate(excelDate) {
+  return new Date(
+    (excelDate - 1) * 24 * 60 * 60 * 1000 + new Date('1900-01-01').getTime(),
+  )
+}
+
+function excelTimeToFormattedTime(excelTime) {
+  if (excelTime < 1) {
+    const hours = Math.floor(excelTime * 24)
+    const minutes = Math.round((excelTime * 24 - hours) * 60)
+    const formattedTime = `${hours
+      .toString()
+      .padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`
+    return formattedTime
+  } else {
+    return '' // Handle other cases as needed
+  }
+}
+
+function excelDateToJSDate(excelDate) {
+  return new Date(
+    (excelDate - 1) * 24 * 60 * 60 * 1000 + new Date('1900-01-01').getTime(),
+  )
+}
+
 app.get('/api/excel', (req, res) => {
-  console.log('hell from api excel')
+  console.log('Hello from api excel')
   // Define the path to your XLSX file
   const filePath = path.join(__dirname, '/timeline_sample.xlsx')
 
@@ -39,8 +64,38 @@ app.get('/api/excel', (req, res) => {
   const sheetName = workbook.SheetNames[0]
   const worksheet = workbook.Sheets[sheetName]
 
-  // Convert the worksheet to JSON data
-  const excelData = XLSX.utils.sheet_to_json(worksheet)
+  // Convert the worksheet to an array of objects, preserving order
+  const excelData = XLSX.utils.sheet_to_json(worksheet, {
+    header: 1,
+    raw: true,
+  })
+
+  // Remove the header row if it exists
+  if (excelData.length > 0 && Array.isArray(excelData[0])) {
+    excelData.shift()
+  }
+
+  // Convert Excel date serial numbers to Date objects
+  const dateColumns = ['DateColumn1', 'DateColumn2'] // Replace with actual column names
+  excelData.forEach((row) => {
+    dateColumns.forEach((columnName) => {
+      const columnIndex = excelData[0].indexOf(columnName)
+      if (columnIndex !== -1) {
+        row[columnIndex] = excelDateToJSDate(row[columnIndex])
+      }
+    })
+  })
+
+  // Convert Excel time fractions to formatted time strings
+  const timeColumns = ['TimeColumn1', 'TimeColumn2'] // Replace with actual column names
+  excelData.forEach((row) => {
+    timeColumns.forEach((columnName) => {
+      const columnIndex = excelData[0].indexOf(columnName)
+      if (columnIndex !== -1) {
+        row[columnIndex] = excelTimeToFormattedTime(row[columnIndex])
+      }
+    })
+  })
 
   // Send the JSON data as a response
   console.log(excelData)
